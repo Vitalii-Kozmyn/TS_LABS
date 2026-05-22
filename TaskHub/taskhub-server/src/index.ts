e754ab80db4552eb7fc3a97e06b14c0544c94275
Vitalii-Kozmyn <157843660+Vitalii-Kozmyn@users.noreply.github.com>
2026-05-22 11:39:51 +0300

    Use CLIENT_URL for CORS origin

--- TaskHub/taskhub-server/src/index.ts
+++ TaskHub/taskhub-server/src/index.ts
@@ -13,8 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: CLIENT_URL,
     credentials: true
 }));
 
